e3232bfa8a7b8fd24860055f5357b048b5dd8acd
Khang Nguyen <khangnd2014@gmail.com>
2024-01-22 15:25:28 +0700

    Introduction Java and Exercises

---
+++ Javacore/Bài tập/HelloNestech.java
@@ -0,0 +1,11 @@
+public class HelloNestech {
+    public static void main(String[] args){
+      //  System.out.print("Xin chao cac ban den voi khoa hoc Java K30\n");
+      int numbersOfStudentK30 = 10;
+      int numbersOfStudentK31 = 15;
+      int numbersOfStudentK35 = 20;
+      int total = numbersOfStudentK30 + numbersOfStudentK31 + numbersOfStudentK35;
+      System.out.println("Total nubmers students is: " + total);
+
+    }
+}
