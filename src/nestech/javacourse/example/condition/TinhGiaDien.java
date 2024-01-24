63bb2c591232541880f1a9bb6394a38fada1eaad
Khang Nguyen <khangnd2014@gmail.com>
2024-01-24 19:29:14 +0700

    update code

--- src/nestech/javacourse/example/condition/TinhGiaDien.java
+++ src/nestech/javacourse/example/condition/TinhGiaDien.java
@@ -28,15 +28,7 @@ public class TinhGiaDien {
     // Phương thức tính giá điện
     public static double tinhGiaDien(double soKWh) {
         double giaDien = 0;
-        if (soKWh <= 50) {
-            giaDien = soKWh * 1500;
-        } else if (soKWh <= 100) {
-            giaDien = 50 * 1500 + (soKWh - 50) * 2000;
-        } else if (soKWh <= 200) {
-            giaDien = 50 * 1500 + 50 * 2000 + (soKWh - 100) * 2500;
-        } else {
-            giaDien = 50 * 1500 + 50 * 2000 + 100 * 2500 + (soKWh - 200) * 3000;
-        }
+        // Implement your code here
         return giaDien;
     }
 }
