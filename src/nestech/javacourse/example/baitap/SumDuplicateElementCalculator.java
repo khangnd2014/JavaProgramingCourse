edf320e1505e5a4dd831404305f8211e91419173
XIVIXMMI <lethaikhoinguyen@gmail.com>
2024-01-29 19:03:46 +0700

    first commit

--- src/nestech/javacourse/example/baitap/SumDuplicateElementCalculator.java
+++ src/nestech/javacourse/example/baitap/SumDuplicateElementCalculator.java
@@ -18,7 +18,14 @@ public class SumDuplicateElementCalculator {
 
     public static int calculateCommonElementsSum(int[] arr1, int[] arr2) {
         int sum = 0;
-        // Implement your code here
+        for(int i = 0; i < arr1.length; i++){
+            for( int j = 0; j < arr2.length; j++){
+                if(arr1[i] == arr2[j]){
+                    sum += arr1[i] + arr2[j];
+                    break;
+                }
+            }
+        }
         return sum;
     }
 
